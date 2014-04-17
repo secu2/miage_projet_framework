487ce0c88ad1bde23b3bc916df8779733e369c77
younesd <younes.dekkoune@gmail.com>
2014-04-17 15:12:55 +0200

    création des constructeurs de la BD + méthode charger un fichier sql

--- src/systeme/BaseDeDonnees.java
+++ src/systeme/BaseDeDonnees.java
@@ -1,6 +1,7 @@
 package systeme;
 import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,20 +20,19 @@ import java.util.Scanner;
 
 public class BaseDeDonnees {
 	
+	// Objet connection permettant l'accès à la base de données
 	Connection connection;
 	
 
 	/**
-	 * Créer une base de données sqlite de nom 'nomFichBD'.db et renvoie son objet Connection
-	 * @param nomFichBD
+	 * Créer/Charge une base de données sqlite de nom 'nomFichBD'.sqlite et renvoie son objet Connection
+	 * @param nomFichBD : nom de la base de données
 	 * @return connection : objet de type Connection permettant la manipulation de la base de données
 	 * @throws ClassNotFoundException
 	 * @throws IOException 
 	 */
 	public BaseDeDonnees(String nomFichBD) throws ClassNotFoundException, IOException {
 		Class.forName("org.sqlite.JDBC");
-				
-		
 		try {
 			connection = DriverManager.getConnection("jdbc:sqlite:" + nomFichBD + ".sqlite");
 		} catch (SQLException e) {
@@ -42,19 +42,58 @@ public class BaseDeDonnees {
 		
 	}
 	
-	public void chargerFichierSQL(String path) throws FileNotFoundException, SQLException{
-		File fichierSQL = new File(path);
-		Scanner scanner = new Scanner(fichierSQL);
-		String sql = new String();
-		while(scanner.hasNext()){
-			sql += " "+scanner.next();
+	
+	/**
+	 * Créer/Charge une base de données sqlite ayant pour nom database.sqlite
+	 * et l'initialise avec la structure de données par défaut si elle n'existe pas
+	 * @return connection : objet de type Connection permettant la manipulation de la base de données
+	 * @throws ClassNotFoundException
+	 * @throws IOException 
+	 * @throws SQLException 
+	 */
+	public BaseDeDonnees() throws ClassNotFoundException, IOException, SQLException {
+		Class.forName("org.sqlite.JDBC");
+		boolean newDB = true;
+		File dbFile = new File("database.sqlite");
+		// test l'existence du fichier database.sqlite avant le chargement avec le driverManager
+		if(dbFile.exists()){
+			newDB = false;
 		}
+		
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:database.sqlite"); //Reminder: cette ligne cree automatiquement le fichier si il n'éxiste pas
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(newDB){
+			this.chargerFichierSQL("data_framework_structure.sqlite");
+		}
+		
+	}
+	
+	/**
+	 * Méthode permettant de charger dans la base de données, le fichier sql situé à path
+	 * @param path
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	public void chargerFichierSQL(String path) throws SQLException, IOException{
+		InputStream inputstream = new FileInputStream(path);
+		String sql = new String();
+		int data = inputstream.read();
+		while(data != -1) {
+		  sql += (char)data;
+		  data = inputstream.read();
+		}
+		inputstream.close();
+		
 		Statement statement = connection.createStatement();
 		System.out.println(sql);
 		statement.executeUpdate(sql);
 		statement.close();
-		connection.close();
-		
+		connection.close();	
 	}
 	
 	/**
@@ -65,11 +104,16 @@ public class BaseDeDonnees {
 	 * @throws ClassNotFoundException 
 	 * @throws SQLException */
 	
+	/**
+	 * Méthode de test de la BD
+	 * @param args
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 * @throws SQLException
+	 */
 	public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
 		BaseDeDonnees bd = new BaseDeDonnees("database");
 		bd.chargerFichierSQL("data_framework_structure.sqlite");
-		
-		
 	}
 	
 	
