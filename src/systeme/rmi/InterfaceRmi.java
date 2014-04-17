3a3c4169e3fd7d8cf84873675a896af92473c365
galletjo <jordan.gallet38@gmail.com>
2014-04-17 10:43:21 +0200

    Mise en place du serveur partie 1 ;)

---
+++ src/systeme/rmi/InterfaceRmi.java
@@ -0,0 +1,13 @@
+package systeme.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+//création de l'interface propre au RMI
+
+public interface InterfaceRmi extends Remote {
+	 
+	public String getTest() throws RemoteException;
+	 
+}
