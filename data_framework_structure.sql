50e57ee641c61a9b81799c3a02c1f23dcf097b32
Clément BROCARD <clementbrocard@free.fr>
2014-04-17 15:23:20 +0200

    modif structure db

--- data_framework_structure.sql
+++ data_framework_structure.sql
@@ -1,5 +1,5 @@
-CREATE TABLE "fichier" ("fichier_id" INTEGER PRIMARY KEY  NOT NULL );
-CREATE TABLE "groupes" ("id_groupe" INTEGER PRIMARY KEY  NOT NULL  UNIQUE , "groupname" VARCHAR DEFAULT noname);
+CREATE TABLE "fichier" ("id_fichier" INTEGER PRIMARY KEY  NOT NULL );
+CREATE TABLE "groupes" ("id_groupe" INTEGER PRIMARY KEY  NOT NULL  UNIQUE , "nom_groupe" VARCHAR DEFAULT noname);
 CREATE TABLE "permissions_groupes" ("id_groupe" INTEGER, "flag" VARCHAR, "value" INTEGER);
 CREATE TABLE "permissions_utilisateurs" ("id_utilisateur" INTEGER, "flag" VARCHAR, "value" INTEGER);
 CREATE TABLE "utilisateurs" ("id_utilisateur" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "nom_utilisateur" VARCHAR);
