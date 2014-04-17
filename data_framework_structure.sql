659329f411b5f1569883baf373ea991cae1ec615
Clément BROCARD <clementbrocard@free.fr>
2014-04-17 16:21:08 +0200

    modif structure (noms de tables au singulier, fumier de younes)

--- data_framework_structure.sql
+++ data_framework_structure.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "fichier" ("id_fichier" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL );
-CREATE TABLE "utilisateurs" ("id_utilisateur" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "nom_utilisateur" VARCHAR  NOT NULL  UNIQUE , "mot_de_passe" VARCHAR);
-CREATE TABLE "groupes" ("id_groupe" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "nom_groupe" VARCHAR  NOT NULL  UNIQUE  DEFAULT noname);
+CREATE TABLE "utilisateur" ("id_utilisateur" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "nom_utilisateur" VARCHAR  NOT NULL  UNIQUE , "mot_de_passe" VARCHAR);
+CREATE TABLE "groupe" ("id_groupe" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "nom_groupe" VARCHAR  NOT NULL  UNIQUE  DEFAULT noname);
 CREATE TABLE "apartenance_groupe" ("id_groupe" INTEGER, "id_utilisateur" INTEGER;
 
