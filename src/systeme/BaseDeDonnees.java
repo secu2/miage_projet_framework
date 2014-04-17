0b4ad114453c48f686d3b1e49e4fc492e6aa7e40
Clément BROCARD <clementbrocard@free.fr>
2014-04-17 13:16:45 +0200

    ajout classe bdd

--- src/systeme/BaseDeDonnees.java
+++ src/systeme/BaseDeDonnees.java
@@ -1,5 +1,12 @@
 package systeme;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class BaseDeDonnees {
+	Connection connection;
 	
 }
