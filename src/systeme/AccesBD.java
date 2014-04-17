e1ac133b37946d617708ae252258778536ef8be9
younesd <younes.dekkoune@gmail.com>
2014-04-17 13:00:16 +0200

    classe AccesBD : méthode creerAccesBD

--- src/systeme/AccesBD.java
+++ src/systeme/AccesBD.java
@@ -1,5 +1,28 @@
 package systeme;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AccesBD {
 
+	/**
+	 * Créer une base de données sqlite de nom 'nomFichBD'.db et renvoie son objet Connection
+	 * @param nomFichBD
+	 * @return connection : objet de type Connection permettant la manipulation de la base de données
+	 * @throws ClassNotFoundException
+	 */
+	public Connection creerAccesBD(String nomFichBD) throws ClassNotFoundException {
+		Class.forName("org.sqlite.JDBC");
+		Connection connection = null;
+
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:" + nomFichBD + ".db");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
 }
