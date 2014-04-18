e233e87e481b2ef1a83018cbf02a5d17aa1ffc44
Clément BROCARD <clementbrocard@free.fr>
2014-04-18 11:32:09 +0200

    Début modif publication

--- src/modules/documents/social/Publication.java
+++ src/modules/documents/social/Publication.java
@@ -1,12 +1,31 @@
 package modules.documents.social;
+import java.util.ArrayList;
 import java.util.Date;
+
+import modules.documents.Document;
+import modules.gestionUtilisateur.Utilisateur;
 
 
 
 public class Publication {
     
-    private Date DateP;
-    private int duree;
-    private String visibilite;
+    private Date dateDePublication;
+    private int dureeDeDisponibilite;
+    private ArrayList<Utilisateur> visibilite;
+    private Utilisateur proprietaire;
+    private Document document;
+	
+    
+    
+    public Publication(Date dateDePublication, int dureeDeDisponibilite, ArrayList<Utilisateur> visibilite,
+			Utilisateur proprietaire, Document document) {
+		this.dateDePublication = dateDePublication;
+		this.dureeDeDisponibilite = dureeDeDisponibilite;
+		this.visibilite = visibilite;
+		this.proprietaire = proprietaire;
+		this.document = document;
+	}
+    
+    
 
 }
